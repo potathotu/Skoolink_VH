598c5eca0e50d999a3a74a9b6795b94da43d7ae9
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-16 10:57:46 -1000

    login component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/auth/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: Login,
+    }
+];
