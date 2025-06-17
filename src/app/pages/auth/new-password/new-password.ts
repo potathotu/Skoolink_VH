da4752ef77887921efacedc34dbc084d3014a136
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-17 13:48:15 -1000

    size fixes for password page

--- src/app/pages/auth/new-password/new-password.ts
+++ src/app/pages/auth/new-password/new-password.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-password',
