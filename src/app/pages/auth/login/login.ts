e496964745b0e4d7b4a07f2e7761bd134fdbdc54
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-17 10:20:47 -1000

    added routerlink to link the auth pages

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { InputTextModule } from 'primeng/inputtext';
 
+
 @Component({
   selector: 'app-login',
-  imports: [InputTextModule, ButtonModule, RadioButtonModule],
+  imports: [RouterLink, InputTextModule, ButtonModule, RadioButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
--- src/app/pages/auth/new-password/new-password.ts
+++ src/app/pages/auth/new-password/new-password.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { InputTextModule } from 'primeng/inputtext';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-password',
