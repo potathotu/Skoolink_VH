5bbad9e7add676c5ae4b6833cd04d0abfd5b9f88
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-16 16:01:39 -1000

    new radio button

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-
-
+import { routes } from './app.routes';
 import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonModule],
+  imports: [InputTextModule, ButtonModule, RadioButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
