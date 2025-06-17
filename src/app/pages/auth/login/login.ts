0aef67c255fc06e98862e8714af64bd70820e921
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-17 10:47:44 -1000

    removed useless modules

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { RadioButtonModule } from 'primeng/radiobutton';
 import { InputTextModule } from 'primeng/inputtext';
 
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, InputTextModule, ButtonModule, RadioButtonModule],
+  imports: [RouterLink, InputTextModule, ButtonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
--- src/app/pages/auth/new-password/new-password.ts
+++ src/app/pages/auth/new-password/new-password.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
-import { RadioButtonModule } from 'primeng/radiobutton';
 import { InputTextModule } from 'primeng/inputtext';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-new-password',
-  imports: [InputTextModule, ButtonModule, RadioButtonModule],
+  imports: [InputTextModule, ButtonModule],
   templateUrl: './new-password.html',
   styleUrl: './new-password.scss'
 })
