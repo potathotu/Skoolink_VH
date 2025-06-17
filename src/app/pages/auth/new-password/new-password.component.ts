c3a9e3ebc01bf4122ffd32a75bed8aa6e179dec0
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-17 09:36:40 -1000

    added imports to new password

--- src/app/pages/auth/new-password/new-password.component.ts
+++ src/app/pages/auth/new-password/new-password.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-new-password',
-  imports: [],
+  imports: [InputTextModule, ButtonModule, RadioButtonModule],
   templateUrl: './new-password.component.html',
   styleUrl: './new-password.component.scss'
 })
