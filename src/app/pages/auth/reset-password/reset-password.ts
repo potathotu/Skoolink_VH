ae933be8b5b9ec8f0234c169bf46961784483f5a
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-17 15:21:55 -1000

    added otp module from primeng in rese-password

--- src/app/pages/auth/reset-password/reset-password.ts
+++ src/app/pages/auth/reset-password/reset-password.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputOtpModule } from 'primeng/inputotp';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [ButtonModule, InputTextModule],
+  imports: [ButtonModule, InputTextModule, InputOtpModule],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.scss'
 })
