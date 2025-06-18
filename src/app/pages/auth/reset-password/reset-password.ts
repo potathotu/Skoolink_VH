affa34e5d7803ddbbfc78d4b305d76b7cd5804c2
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-17 21:09:07 -1000

    insert code page added

--- src/app/pages/auth/reset-password/reset-password.ts
+++ src/app/pages/auth/reset-password/reset-password.ts
@@ -3,15 +3,21 @@ import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputOtpModule } from 'primeng/inputotp';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [ButtonModule, InputTextModule, InputOtpModule],
+  imports: [FormsModule, ButtonModule, InputTextModule, InputOtpModule, CommonModule, RouterLink],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.scss'
 })
 export class ResetPassword {
+  showNewContent = false;
   
+  onClick(){
+    this.showNewContent = true;
+  }
 }
 
 
