28de05ae3da3a4db9990355206bfbf016f572717
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-17 14:31:19 -1000

    added reset-password component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/auth/login/login';
 import { NewPassword } from './pages/auth/new-password/new-password';
+import { ResetPassword } from './pages/auth/reset-password/reset-password';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,10 @@ export const routes: Routes = [
     {
         path:"new-password",
         component: NewPassword,
+    },
+
+    {
+        path:"reset-password",
+        component: ResetPassword,
     }
 ];
