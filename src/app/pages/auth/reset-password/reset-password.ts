5f9897fb1e82d4014e3bf86a4a18294f452f2599
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-17 15:06:42 -1000

    updated reset password interface

--- src/app/pages/auth/reset-password/reset-password.ts
+++ src/app/pages/auth/reset-password/reset-password.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  imports: [RouterLink, ButtonModule, InputTextModule],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.scss'
 })
