14e1845a83ce5b34de300dd03bb424a5db854889
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-20 14:42:42 -1000

    recentered thematique blocks

--- src/app/shared/layout/navbar/navbar.ts
+++ src/app/shared/layout/navbar/navbar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [ButtonModule, RouterModule],
+  imports: [ButtonModule, RouterModule, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
