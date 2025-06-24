0c29252412ebf364348d572734faea2ae4cbeae6
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-24 11:16:40 -1000

    generated new messagerie component

---
+++ src/app/pages/messagerie/messagerie.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messagerie',
+  imports: [],
+  templateUrl: './messagerie.component.html',
+  styleUrl: './messagerie.component.scss'
+})
+export class MessagerieComponent {
+
+}
