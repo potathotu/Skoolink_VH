af09755f7402bdd65f842b756b3c2689ffbb8868
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-24 13:39:38 -1000

    updated project interface

--- src/app/pages/projets/projets.ts
+++ src/app/pages/projets/projets.ts
@@ -13,74 +13,66 @@ export class Projets {
   
   projets: any = [
     {
-      number: 6,
+      number: 5,
       img: "assets/meteo.png",
       name: "Application de météo",
       startAt: "10 juin",
       endAt: "26 juin",
-      status: "En cours",
+      status: "Terminé",
       link: "https://angular.dev/",
-      thematique: 1
+      thematique: 1,
+      thematiqueImage: "assets/angular-logo.png"
     },
 
     {
-      number: 5,
+      number: 4,
       img: "assets/rps.png",
       name: "Pierre-Papier-Ciseaux",
       startAt: "22 mai",
       endAt: "10 juin",
       status: "Terminé",
       link: "https://angular.dev/",
-      thematique: 1
+      thematique: 1,
+      thematiqueImage: "assets/angular-logo.png"
     },
   
     {
-      number: 4,
+      number: 3,
       img: "assets/backlog.png",
       name: "Mettre en place un BackLog",
       startAt: "17 avril",
       endAt: "21 mai",
       status: "Terminé",
       link: "https://app.clickup.com/6918654/home",
-      thematique: 2
-    },
-
-    {
-      number: 3,
-      img: "assets/meteo.png",
-      name: "Application de météo",
-      startAt: "10 juin",
-      endAt: "26 juin",
-      status: "Terminé",
-      link: "https://angular.dev/",
-      thematique: 1
+      thematique: 2,
+      thematiqueImage: "assets/clickup-logo.png"
     },
 
     {
       number: 2,
-      img: "assets/rps.png",
-      name: "Pierre-Papier-Ciseaux",
-      startAt: "22 mai",
-      endAt: "10 juin",
-      status: "Terminé",
-      link: "https://angular.dev/",
-      thematique: 1
-    },
-  
-    {
-      number: 1,
-      img: "assets/backlog.png",
-      name: "Mettre en place un BackLog",
-      startAt: "17 avril",
-      endAt: "21 mai",
+      img: "assets/chao.png",
+      name: "Créer son premier wireframe",
+      startAt: "00 avril",
+      endAt: "00 mai",
       status: "Terminé",
       link: "https://app.clickup.com/6918654/home",
-      thematique: 2
-    }
+      thematique: 2,
+      thematiqueImage: "assets/figma-logo.png"
+    },
 
+    {
+      number: 1,
+      img: "assets/repository.png",
+      name: "Apprendre à utiliser gitlab",
+      startAt: "00 avril",
+      endAt: "00 mai",
+      status: "Terminé",
+      link: "https://app.clickup.com/6918654/home",
+      thematique: 2,
+      thematiqueImage: "assets/gitlab-logo.png"
+    }
   ]
     
-
   constructor(private _pageService: PageService){
     _pageService.title = "Projets"
   }
