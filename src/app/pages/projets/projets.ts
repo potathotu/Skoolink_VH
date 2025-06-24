a94b2d4a8dc85a77d6e106c676fd7039ba2b698b
Hotuanui VONTOR <hotuanui@gmail.com>
2025-06-24 11:15:07 -1000

    remade projets interface to fit all the projects without separators

--- src/app/pages/projets/projets.ts
+++ src/app/pages/projets/projets.ts
@@ -11,51 +11,75 @@ import { ButtonModule } from 'primeng/button';
 })
 export class Projets { 
   
-  thematique: any = [
+  projets: any = [
     {
-      thematique: 1,
-      title: "Introduction à Angular",
-      projets:[
-        {
-          number: 5,
-          img: "assets/meteo.png",
-          name: "Application de météo",
-          startAt: "10 juin",
-          endAt: "26 juin",
-          status: "En cours",
-          link: "https://angular.dev/"
-        },
-
-        {
-          number: 4,
-          img: "assets/rps.png",
-          name: "Pierre-Papier-Ciseaux",
-          startAt: "22 mai",
-          endAt: "10 juin",
-          status: "Terminé",
-          link: "https://angular.dev/"
-        }
-      ]
+      number: 6,
+      img: "assets/meteo.png",
+      name: "Application de météo",
+      startAt: "10 juin",
+      endAt: "26 juin",
+      status: "En cours",
+      link: "https://angular.dev/",
+      thematique: 1
     },
 
     {
-      thematique: 2,
-      title: "Introduction à ClickUp",
-      projets:[
-        {
-          number: 3,
-          img: "assets/backlog.png",
-          name: "Mettre en place un BackLog",
-          startAt: "17 avril",
-          endAt: "21 mai",
-          status: "Terminé",
-          link: "https://app.clickup.com/6918654/home"
-        }
+      number: 5,
+      img: "assets/rps.png",
+      name: "Pierre-Papier-Ciseaux",
+      startAt: "22 mai",
+      endAt: "10 juin",
+      status: "Terminé",
+      link: "https://angular.dev/",
+      thematique: 1
+    },
   
-      ]
+    {
+      number: 4,
+      img: "assets/backlog.png",
+      name: "Mettre en place un BackLog",
+      startAt: "17 avril",
+      endAt: "21 mai",
+      status: "Terminé",
+      link: "https://app.clickup.com/6918654/home",
+      thematique: 2
+    },
+
+    {
+      number: 3,
+      img: "assets/meteo.png",
+      name: "Application de météo",
+      startAt: "10 juin",
+      endAt: "26 juin",
+      status: "Terminé",
+      link: "https://angular.dev/",
+      thematique: 1
+    },
+
+    {
+      number: 2,
+      img: "assets/rps.png",
+      name: "Pierre-Papier-Ciseaux",
+      startAt: "22 mai",
+      endAt: "10 juin",
+      status: "Terminé",
+      link: "https://angular.dev/",
+      thematique: 1
+    },
+  
+    {
+      number: 1,
+      img: "assets/backlog.png",
+      name: "Mettre en place un BackLog",
+      startAt: "17 avril",
+      endAt: "21 mai",
+      status: "Terminé",
+      link: "https://app.clickup.com/6918654/home",
+      thematique: 2
     }
 
-  ];
+  ]
+    
 
   constructor(private _pageService: PageService){
     _pageService.title = "Projets"
